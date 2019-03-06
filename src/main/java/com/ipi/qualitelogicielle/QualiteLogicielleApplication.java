37b78bc0fdc3e68a39ff7c9fb51fbacbc5fb52c5
MonsieurMan <ange.g.picard@gmail.com>
2019-03-06 09:59:03 +0100

    Initial commit

---
+++ src/main/java/com/ipi/qualitelogicielle/QualiteLogicielleApplication.java
@@ -0,0 +1,13 @@
+package com.ipi.qualitelogicielle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualiteLogicielleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualiteLogicielleApplication.class, args);
+	}
+
+}
